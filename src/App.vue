4d338d23c2bb43e0e230c6c489266010411b9192
William Beinö <william.beino@hotmail.com>
2020-07-22 23:58:16 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-    <div id="app">
+  <div id="app">
     <Menu v-if="! roomCode" :socket="socket"/>
     <Game v-else :roomCode="roomCode" :socket="socket" />
+
   </div>
 </template>
 
 <script>
 import io from 'socket.io-client';
-const socket = io('192.168.43.1:3000');
+const socket = io('localhost:3000');
 
 import Menu from '@/components/Menu.vue';
 import Game from '@/components/Game.vue';
@@ -20,6 +21,7 @@ export default {
     }
   },
   created() {
+    this.createCanvas(100, 100);
     socket.on('init', (roomCode) => {
       this.roomCode = roomCode;
     });
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,14 +1,58 @@
 <template>
-  <div class="">
-    <button class="btn" @click="create">nytt spel</button>
-    <div class="input-group col-4">
-      <input v-model="roomCode" type="text" class="form-control" placeholder="#AAA" aria-label="Room code">
-      <div class="input-group-append">
-        <button class="btn" @click="join" type="button">delta</button>
-      </div>
+  <div class="daddy">
+    <h1>dricka.</h1>
+    <button @click="create">Nytt spel</button>
+    <div class="join">
+      <input v-model="roomCode" maxlength="3" type="text" placeholder="#AAA">
+      <button class="half" @click="join" type="button">Delta</button>
     </div>
   </div>
 </template>
+
+<style scoped>
+  h1 {
+    display: inline-block;
+    position: absolute;
+    left: 40px;
+    bottom: calc(50vh - 150px);
+    font-size: 3em;
+    font-weight: 600;
+    color:white;
+  }
+  button {
+    font-size: 1.5em;
+    font-weight: 600;
+    border-radius: calc(47.5px / 2);
+    border: none;
+    padding: 5px 41px;
+    background: #DE38C8;
+    color: #fff;
+  }
+  input {
+    font-size: 1.5em;
+    font-weight: 600;
+    height: 35px;
+    width: 90px;
+    margin: 0px;
+    border: none;
+    border-radius: calc(47.5px / 2) 0px 0px calc(47.5px / 2);
+  }
+  .half {
+    padding: 5px 15px;
+    border-radius: 0px calc(47.5px / 2) calc(47.5px / 2) 0px;
+  }
+  .join {
+    padding-top: 10px;
+    width: 100%;
+  }
+  .daddy {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+  }
+</style>
 
 <script>
 export default {
@@ -24,6 +68,11 @@ export default {
     },
     join () {
       this.socket.emit("join", this.roomCode);
+    }
+  },
+  watch: {
+    roomCode(val) {
+      this.roomCode = val.toUpperCase();
     }
   }
 }
