5533ec9385cb598b699cc1277a2e98ec8abfb43d
Ludvig Berggren <ludvige@hotmail.com>
2020-08-10 20:24:11 +0200

    add vuex store file

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-import io from "socket.io-client";
+// import Vuex from 'vuex';
+// import io from "socket.io-client";
+import store from './store'
 
 import App from './App.vue';
 
 Vue.config.productionTip = false;
-
-Vue.use(Vuex);
-
-var ip = process.env.VUE_APP_IP || '127.0.0.1';
-var port = process.env.VUE_APP_PORT || '3000';
-
-const store = new Vuex.Store({
-  state: {
-    socket: io(`${ip}:${port}`),
-  },
-})
 
 new Vue({
   store,
